3779881133c4c1e6c26ba0d93769bdc5c6cb07e8
jiachenli011 <jli114@syr.edu>
2024-09-12 15:00:31 -0400

    finish main program, test pass 3/4. prase packaging fail v0.3

--- code/program,.py
+++ code/program,.py
@@ -27,3 +27,27 @@ Example:
 '''
 
 # TODO: Write code
+import json
+from packaging import parse_packaging, calc_total_units, get_unit
+
+input_file_path = 'data/packaging.txt'
+output_file_path = 'data/packaging.json'
+
+all_packages = []
+
+with open(input_file_path, 'r') as f:
+
+    for line in f:
+        line = line.strip()
+        if line:
+            package = parse_packaging(line)
+            total_units = calc_total_units(package)
+            unit = get_unit(package)
+            print(f"{line} => total units: {total_units} {unit}")
+
+            all_packages.append(package)
+
+        with open(output_file_path, 'w') as json_file:
+            json.dump(all_packages, json_file, indent=4)
+
+        print(f"Packaging data saved to {output_file_path}")
